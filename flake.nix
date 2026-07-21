24dae2e425a5ca184a981b396c0305ea5e3fb48b
Mike O'Driscoll <mike@unusedbytes.ca>
2026-07-20 20:43:57 -0400

    flake,go.mod: update to go 1.26.5

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Intel GPU Exporter for Prometheus";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     systems.url = "github:nix-systems/default";
   };
 
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/mikeodr/intel-gpu-exporter-go
 
-go 1.26.4
+go 1.26.5
 
 require (
 	github.com/frankban/quicktest v1.14.6
